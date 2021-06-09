6af019b6c20ca6b49ca70c981cc156efda60f04d
HUe1998 <himanshu.1998me@gmail.com>
2021-06-09 22:36:02 +0530

    Complete Login View

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from django.urls import path
+from django.urls import path, include
 from .views import dashboardView
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('dashboard/', dashboardView, name='dashboard'),
 ]
